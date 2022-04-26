5d58c3885e9698fe1a3342acbc501940a06ef8f0
Dor.M <dor@mov.ai>
2022-04-26 18:14:50 +0300

    added deps

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requiremets = [
+    "aiohttp==3.6.2",
+    "aiohttp-cors==0.7.0",
+    "aioredis==1.3.0",
+    "uvloop==0.14.0",
+    "dal==1.0.0.23",
+    "movai_core_shared==1.0.0.8",
+    "gd_node==1.0.0.6",
+]
+
 # TODO Adapt your project configuration to your own project.
 # The name of the package is the one to be used in runtime.
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
@@ -18,6 +28,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
-    install_requires=["dal==1.0.0.23", "movai_core_shared==1.0.0.8"],
+    install_requires=requiremets,
     entry_points={},
 )
