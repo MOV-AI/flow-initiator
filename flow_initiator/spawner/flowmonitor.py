92ce964e0a64c88d2d242f60778da07837d5930f
erezz-mov-ai <94361921+erezz-mov-ai@users.noreply.github.com>
2022-09-28 12:52:17 +0300

    Feature bp 741 replace movai classes with flow initiator (#29)
    * update import to full path for models and scopes
    
    * replaced by  dal.scopes.container
    
    * replaced by dal.scopes.flowlinks
    
    * replaced by dal.models.node
    
    * removed models folder from flow_initiator
    
    * reorgenize imports
    
    * moved from movai_classes
    
    * removed because it's duplicated with dal.helpers.helpers
    
    * update import path
    
    * cancel changes that I did on flowmonitor
    
    * updating scopestree import path
    
    * fix missing arg to logger.error
    
    * restore to relative import

--- flow_initiator/spawner/flowmonitor.py
+++ flow_initiator/spawner/flowmonitor.py
@@ -11,13 +11,7 @@ import re
 import copy
 import rosparam
 from types import SimpleNamespace
-
 import os
-from dal.scopes import scopes
-from dal.helpers.parsers import ParamParser
-
-# for typing
-from dal.models import Flow
 
 from movai_core_shared.consts import (
     ROS1_NODELET,
@@ -45,6 +39,12 @@ from movai_core_shared.envvars import (
 )
 
 from movai_core_shared.logger import Log
+
+from dal.models.scopestree import scopes
+from dal.models.flow import Flow
+from dal.helpers.parsers import ParamParser
+
+
 
 LOGGER = Log.get_logger("spawner.mov.ai")
 
