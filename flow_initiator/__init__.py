d275876570779e4f407ae2de4dde9397a3f57bf6
Erez Zomer <erez@mov.ai>
2023-04-17 09:46:52 +0300

    filter DeprecationWarnings

--- flow_initiator/__init__.py
+++ flow_initiator/__init__.py
@@ -10,9 +10,14 @@
 """
 import signal
 import argparse
+import os
+import warnings
+
 from dal.movaidb import RedisClient
 from .spawner import Core
 
+warnings.simplefilter("ignore", category=DeprecationWarning)
+os.environ["PYTHONWARNINGS"] = "ignore"
 
 def main():
     """spawner entrypoint"""
