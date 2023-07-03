9c4852fbe95907865fb962cbffbfc33e9a8754e6
M Mograbi <Mograbi@users.noreply.github.com>
2023-07-03 14:58:40 +0300

    pump dal version (#82)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
     "aioredis==1.3.1",
     "uvloop==0.14.0",
     "movai-core-shared==2.4.1.16",
-    "data-access-layer==2.4.1.29",
+    "data-access-layer==2.4.1.30",
     "gd-node==2.4.1.16",
 ]
 
