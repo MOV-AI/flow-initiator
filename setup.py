4aa5fa9098f29320ad8386d691d19be5b5f86133
Erez Zomer <erez@mov.ai>
2023-11-09 09:35:46 +0200

    Version 2.5.0.0

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r") as fh:
 requirements = [
     "aioredis==1.3.1",
     "uvloop==0.14.0",
-    "movai-core-shared==2.5.0.0",
-    "data-access-layer==2.5.0.0",
-    "gd-node==2.5.0.0",
+    "movai-core-shared==2.5.0.1",
+    "data-access-layer==2.5.0.1",
+    "gd-node==2.5.0.1",
 ]
 
 
