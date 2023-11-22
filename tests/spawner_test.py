96fcf9369f21297ed099fcf3b35c66d3bbe7e30f
Erez Zomer <erez@mov.ai>
2023-11-22 19:40:07 +0000

    fix spawner_test, missing commands for validator

--- tests/spawner_test.py
+++ tests/spawner_test.py
@@ -12,13 +12,17 @@ import unittest
 
 from movai_core_shared.exceptions import CommandError, ActiveFlowError
 
-from flow_initiator.spawner.validation import CommandValidator
+from dal.scopes import Robot
 
+from flow_initiator.spawner.validation import CommandValidator
+from flow_initiator.spawner.spawner import Spawner
 
 class TestSpawner(unittest.TestCase):
     """
     Test spawner
     """
+
+    spawner = Spawner(Robot())
 
     def test_command_validation(self):
         """
@@ -39,7 +43,32 @@ class TestSpawner(unittest.TestCase):
             "EMERGENCY_UNSET",
         ]
 
-        validator = CommandValidator()
+        validator = CommandValidator(
+            {
+                "START": [self.spawner.process_start, "command='START', flow='flow_name'"],
+                "STOP": [self.spawner.process_stop, "command='STOP', flow='flow_name'"],
+                "TRANS": [
+                    self.spawner.process_transition,
+                    "command='TRANS', node='node_name', port='port_name'",
+                ],
+                "LOCK": [self.spawner.process_lock, "command='LOCK', data={data}"],
+                "UNLOCK": [self.spawner.process_unlock, "command='UNLOCK', data={data}"],
+                "RUN": [self.spawner.process_run, "command='RUN', node='node_name'"],
+                "KILL": [
+                    self.spawner.process_kill,
+                    "command='KILL', node='node_name', dependencies=bool",
+                ],
+                "HELP": [self.spawner.process_help, "command='HELP'"],
+                "EMERGENCY_SET": [
+                    self.spawner.process_emergency_set,
+                    "command='EMERGENCY_SET', data={'nodes_to_skip': [<regex>]}",
+                ],
+                "EMERGENCY_UNSET": [
+                    self.spawner.process_emergency_unset,
+                    "command='EMERGENCY_UNSET'",
+                ],
+            }
+        )
 
         with self.subTest():
             command = {"command": "TRANS", "active_flow": None}
