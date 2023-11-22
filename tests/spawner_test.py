9174e338d31ce9c031f6a336121ffcc50661d12b
Erez Zomer <erez@mov.ai>
2023-11-22 20:03:35 +0000

    remove command_dict

--- tests/spawner_test.py
+++ tests/spawner_test.py
@@ -42,32 +42,7 @@ class TestSpawner(unittest.TestCase):
             "EMERGENCY_UNSET",
         ]
 
-        validator = CommandValidator(
-            {
-                "START": [self.spawner.process_start, "command='START', flow='flow_name'"],
-                "STOP": [self.spawner.process_stop, "command='STOP', flow='flow_name'"],
-                "TRANS": [
-                    self.spawner.process_transition,
-                    "command='TRANS', node='node_name', port='port_name'",
-                ],
-                "LOCK": [self.spawner.process_lock, "command='LOCK', data={data}"],
-                "UNLOCK": [self.spawner.process_unlock, "command='UNLOCK', data={data}"],
-                "RUN": [self.spawner.process_run, "command='RUN', node='node_name'"],
-                "KILL": [
-                    self.spawner.process_kill,
-                    "command='KILL', node='node_name', dependencies=bool",
-                ],
-                "HELP": [self.spawner.process_help, "command='HELP'"],
-                "EMERGENCY_SET": [
-                    self.spawner.process_emergency_set,
-                    "command='EMERGENCY_SET', data={'nodes_to_skip': [<regex>]}",
-                ],
-                "EMERGENCY_UNSET": [
-                    self.spawner.process_emergency_unset,
-                    "command='EMERGENCY_UNSET'",
-                ],
-            }
-        )
+        validator = CommandValidator({})
 
         with self.subTest():
             command = {"command": "TRANS", "active_flow": None}
