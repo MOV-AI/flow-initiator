9756a8f257d1b673bb0b4bd522112524172db108
Erez Zomer <erez@mov.ai>
2023-11-27 10:42:44 +0200

    remove aioredis from setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 requirements = [
-    "aioredis==1.3.1",
-    "uvloop==0.14.0",
     "movai-core-shared==2.4.1.36",
     "data-access-layer==2.4.1.36",
     "gd-node==2.4.1.21",
 ]
 
+# aioredis is required by data-access-layer
 
 setuptools.setup(
     name="flow-initiator",
