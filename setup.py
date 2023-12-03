b6204f34204f9cfdf387e71045181ce04f62c807
Erez Zomer <erez@mov.ai>
2023-12-03 17:36:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ requirements = [
     "aioredis==1.3.1",
     "uvloop==0.14.0",
     "docker==6.1.0",
-    "movai-core-shared==2.5.0.4",
-    "data-access-layer==2.5.0.5",
-    "gd-node==2.5.0.4"
+    "movai-core-shared==2.5.0.6",
+    "data-access-layer==2.5.0.6",
+    "gd-node==2.5.0.5"
 ]
 
 
