d6b12caa681ee9e9303c3a12af3e737184ff32aa
OttoMation-Movai <automation@mov.ai>
2023-12-03 15:57:23 +0000

    [skip actions] Automatic Bump of build version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setuptools.setup(
     name="flow-initiator",
-    version="2.5.0-5",
+    version="2.5.0-6",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Dummy description",
