8acae3c3d927c87f6f6e3f81f729e4d04d79e26c
Dor.M <dor@mov.ai>
2024-02-01 17:43:33 +0200

    bumping version manually

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setuptools.setup(
     name="flow-initiator",
-    version="2.4.1-51",
+    version="2.4.1-53",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Dummy description",
