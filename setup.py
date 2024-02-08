f57540718a30294c46231c42c9626d9d29a1f1f4
Alex Fernandes <alex.fernandes@mov.ai>
2024-02-08 15:47:01 +0000

    updating dal and core-shared

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ requirements = [
     "aioredis==1.3.1",
     "uvloop==0.14.0",
     "docker==6.1.2",
-    "movai-core-shared==2.5.0.6",
-    "data-access-layer==2.5.0.6",
+    "movai-core-shared==2.5.0.12",
+    "data-access-layer==2.5.0.10",
     "gd-node==2.5.0.6"
 ]
 
