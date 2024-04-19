c492d6f54de588ea27f0dcadfbcfff9158cf917f
duartecoelhomovai <duarte@mov.ai>
2024-04-19 09:34:04 +0100

    BP-1194

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
     "uvloop==0.14.0",
     "docker==6.1.2",
     "movai-core-shared==2.5.0.14",
-    "data-access-layer==2.5.0.14",
+    "data-access-layer==2.5.0.15",
     "gd-node==2.5.0.10"
 ]
 
