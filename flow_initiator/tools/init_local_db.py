232a8f439537c6a7ab701e1f6f8dd618796b4e28
Mithun Kinarullathil <51120171+MithunKinarullathil@users.noreply.github.com>
2024-05-20 15:32:15 +0100

    Disable the line that collects the module information temporarily. This part of the code needs to be refactored to not run any code while collecting their information.

--- flow_initiator/tools/init_local_db.py
+++ flow_initiator/tools/init_local_db.py
@@ -21,7 +21,7 @@ from dal.models.message import Message
 def main():
     """initialize redis local db"""
     Message.export_portdata(db="local")
-    Callback.export_modules()
+    # Callback.export_modules()
     # TODO: this is a bit overkill, we should change how we are getting all the python packages/modules (without actually executing)
     # export module is launching a set of processes that might run unwanted code => Security risk
     os.kill(os.getpid(), signal.SIGKILL)
