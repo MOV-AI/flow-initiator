b6a5621c7b707cb06fa27e0a84486c47e02e3371
duartecoelhomovai <84720623+duartecoelhomovai@users.noreply.github.com>
2024-09-13 16:35:10 +0100

    Bump movai-core-shared

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
     "aioredis==1.3.1",
     "uvloop==0.14.0",
     "docker==6.1.2",
-    "movai-core-shared==2.5.0.17",
+    "movai-core-shared==2.5.0.18",
     "data-access-layer==2.5.0.27",
     "gd-node==2.5.0.18"
 ]
